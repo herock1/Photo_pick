f5b5112edda41a2dcb99ade8bf15550baadc757d
Mehedi Hasan <herocksust@gmail.com>
2015-04-02 16:55:48 +0600

    Initial Commit

---
+++ Photo_pick/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Photo_pick
+//
+//  Created by Mehedi Hasan on 4/2/15.
+//  Copyright (c) 2015 Mehedi Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Photo_pick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photo_pick
+//
+//  Created by Mehedi Hasan on 4/2/15.
+//  Copyright (c) 2015 Mehedi Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
